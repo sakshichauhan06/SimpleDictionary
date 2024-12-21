228fdaab4f57aae879fe9368ce466b756711a6e7
Sakshi <chauhansakshi4m@gmail.com>
2024-12-21 23:44:25 +0530

    Added API call

--- app/src/main/java/com/example/simpledictionary/MainActivity.kt
+++ app/src/main/java/com/example/simpledictionary/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.simpledictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.simpledictionary.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchBtn.setOnClickListener {
+            val word = binding.searchField.text.toString()
+            getMeaning(word)
+        }
+    }
+
+    private fun getMeaning(word : String) {
+        GlobalScope.launch {
+            val response = RetrofitInstance.dictionaryApi.getMeaning(word)
+            Log.i("Response from API", response.body().toString())
+        }
     }
 }
--- app/src/main/java/com/example/simpledictionary/RetrofitInstance.kt
+++ app/src/main/java/com/example/simpledictionary/RetrofitInstance.kt
@@ -1,4 +1,19 @@
 package com.example.simpledictionary
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+
+    private const val BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/"
+
+    private fun getInstance() : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val dictionaryApi : DictionaryApi = getInstance().create(DictionaryApi::class.java)
+
 }
